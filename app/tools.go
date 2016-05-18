7ff6ba05c2dc46e9b27ca654f9f46840d519c610
Bogdan Popa <popa.bogdanp@gmail.com>
2016-05-18 10:50:11 +0300

    provision more users

--- app/tools.go
+++ app/tools.go
@@ -20,7 +20,7 @@ func provisionHandler(res http.ResponseWriter, req *http.Request, _ httprouter.P
 	}
 
 	ctx := appengine.NewContext(req)
-	company, _, err := models.CreateMainUser(
+	company, user, err := models.CreateMainUser(
 		ctx, "defn", "defn",
 		"Bogdan Popa", "bogdan@defn.io", "password", "Europe/Bucharest",
 	)
@@ -28,26 +28,44 @@ func provisionHandler(res http.ResponseWriter, req *http.Request, _ httprouter.P
 		return
 	}
 
+	user.Workdays = models.Workdays{
+		models.Workday{Start: 15, End: 23},
+		models.Workday{Start: 15, End: 23},
+		models.Workday{Start: 15, End: 23},
+		models.Workday{Start: 15, End: 23},
+		models.Workday{Start: 13, End: 21},
+		models.Workday{Start: 0, End: 0},
+		models.Workday{Start: 0, End: 0},
+	}
+	user.Put(ctx)
+
 	users := []struct {
 		Name     string
 		Email    string
-		Password string
 		Timezone string
 	}{
-		{"Casey Muratori", "casey@mollyrocket.com", "password", "US/Pacific"},
-		{"Sorin Muntean", "me@sorinmuntean.ro", "password", "Europe/Berlin"},
-		{"Dave Hayes", "dave@ave81.com", "password", "US/Central"},
-		{"John Watson", "john@defn.io", "password", "Europe/London"},
-		{"Simon Peyton Jones", "simonpj@microsoft.com", "password", "Europe/London"},
-		{"Paul Popa", "paul@defn.io", "password", "Europe/Bucharest"},
-		{"Radu Dan", "radu@fullthrottle.ro", "password", "Europe/Bucharest"},
-		{"Rob Pike", "rob@google.com", "password", "US/Central"},
+		{"Casey Muratori", "casey@mollyrocket.com", "US/Pacific"},
+		{"Sorin Muntean", "me@sorinmuntean.ro", "Europe/Berlin"},
+		{"Dave Hayes", "dave@ave81.com", "US/Central"},
+		{"Ben Demaree", "ben.demaree@ave81.com", "US/Central"},
+		{"John Watson", "john@defn.io", "Europe/London"},
+		{"Simon Peyton Jones", "simonpj@microsoft.com", "Europe/London"},
+		{"Paul Popa", "paul@defn.io", "Europe/Bucharest"},
+		{"Radu Dan", "radu@fullthrottle.ro", "Europe/Bucharest"},
+		{"Andrei Baidoc", "baidoc@yahoo.co.uk", "Europe/Bucharest"},
+		{"Rob Pike", "rob@google.com", "US/Central"},
+		{"Gary Bernhardt", "support@destroyallsoftware.com", "US/Pacific"},
+		{"Mike Acton", "me@macton.ninja", "US/Pacific"},
+		{"Ryan Singer", "ryan@singer.com", "US/Central"},
+		{"Joshua Stein", "jcs@jcs.org", "US/Central"},
+		{"Andrew Clarkson", "andrew.clarkson@ave81.com", "US/Central"},
+		{"Dave Firnstahl", "dave.firnstahl@ave81.com", "US/Central"},
 	}
 
 	for _, user := range users {
 		models.CreateUser(
 			ctx, company.Key(ctx),
-			user.Name, user.Email, user.Password, user.Timezone,
+			user.Name, user.Email, "password", user.Timezone,
 		)
 	}
 }
