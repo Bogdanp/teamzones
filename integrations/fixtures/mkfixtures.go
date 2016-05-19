ffa7e62ae683526ab315ea10edeafeb833a5fa55
Bogdan Popa <popa.bogdanp@gmail.com>
2016-05-19 23:32:32 +0300

    save token after teardown in gcalendar tests

--- integrations/fixtures/mkfixtures.go
+++ integrations/fixtures/mkfixtures.go
@@ -31,8 +31,7 @@ func loadCalendarConfig() *oauth2.Config {
 
 func calendarTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
+	fmt.Printf("Go to the following link in your browser then type the authorization code: \n%v\n", authURL)
 
 	var code string
 	if _, err := fmt.Scan(&code); err != nil {
--- integrations/gcalendar_test.go
+++ integrations/gcalendar_test.go
@@ -1,32 +1,16 @@
 package integrations
 
 import (
+	"flag"
 	"log"
 	"os"
 	"testing"
-
-	"google.golang.org/api/calendar/v3"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 )
 
 var calendarToken *oauth2.Token
-
-func init() {
-	fixture := "fixtures/gcalendar_token.json"
-	f, err := os.Open(fixture)
-	if err != nil {
-		log.Fatalf("Fixture not found: %v", err)
-	}
-
-	calendarToken, err = TokenFromJSON(f)
-	if err != nil {
-		log.Fatalf("Failed to read fixture: %v", err)
-	}
-
-	defer f.Close()
-}
 
 func TestCalendarConfig(t *testing.T) {
 	if calendarConfig == nil {
@@ -41,9 +25,43 @@ func TestCalendarService(t *testing.T) {
 		t.Fail()
 	}
 
-	listService := calendar.NewCalendarListService(service)
-	_, err = listService.List().Do()
+	_, err = service.CalendarList.List().Do()
 	if err != nil {
-		t.Fail()
+		t.Fatalf("error fetching calendars: %v", err)
 	}
+}
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	// Load the token
+	fixture := "fixtures/gcalendar_token.json"
+	f, err := os.Open(fixture)
+	if err != nil {
+		log.Fatalf("Fixture not found: %v", err)
+	}
+	defer f.Close()
+
+	calendarToken, err = TokenFromJSON(f)
+	if err != nil {
+		log.Fatalf("Failed to read fixture: %v", err)
+	}
+
+	// Run the tests
+	res := m.Run()
+
+	// Re-save the token
+	of, err := os.Create(fixture)
+	if err != nil {
+		log.Fatalf("Failed to recreate fixture: %v", err)
+	}
+	defer of.Close()
+
+	json, err := TokenToJSON(calendarToken)
+	if err != nil {
+		log.Fatalf("Failed to convert token to JSON: %v", err)
+	}
+	of.Write(json)
+
+	os.Exit(res)
 }
