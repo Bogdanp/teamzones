21472501ffd93631c2b8558528486cfb633d4bcf
Bogdan Popa <popa.bogdanp@gmail.com>
2016-06-02 14:40:09 +0300

    fix tests

--- app/sitemap_test.go
+++ app/sitemap_test.go
@@ -18,10 +18,10 @@ func TestBuilding(t *testing.T) {
 	}{
 		{newBuilder("/sign-up/:invite").Param("invite", "12312312312"), "/sign-up/12312312312"},
 		{newBuilder("/hello/:a/b/c/:d/e/f/g").Param("a", "a").Param("d", "d"), "/hello/a/b/c/d/e/f/g"},
-		{newBuilder("/hello/:a/b/c/:d/e/f/g").Param("a", "a").Param("d", "d").Subdomain("test"), "http://test.teamzones.io/hello/a/b/c/d/e/f/g"},
+		{newBuilder("/hello/:a/b/c/:d/e/f/g").Param("a", "a").Param("d", "d").Subdomain("test"), "http://test.teamzones.dev:8080/hello/a/b/c/d/e/f/g"},
 		{newBuilder("/oauth-callback").Query("code", "abc").Query("state", "def").Query("error", ""), "/oauth-callback?code=abc&state=def&error="},
-		{newBuilder("/sign-in").Query("return", "http://foo.teamzones.io"), "/sign-in?return=http%3A%2F%2Ffoo.teamzones.io"},
-		{newBuilder("/sign-in").Subdomain("test"), "http://test.teamzones.io/sign-in"},
+		{newBuilder("/sign-in").Query("return", "http://foo.teamzones.dev:8080"), "/sign-in?return=http%3A%2F%2Ffoo.teamzones.dev%3A8080"},
+		{newBuilder("/sign-in").Subdomain("test"), "http://test.teamzones.dev:8080/sign-in"},
 	}
 
 	for _, test := range cases {
--- utils/vat_test.go
+++ utils/vat_test.go
@@ -1,13 +1,20 @@
 package utils
 
 import (
+	"net/http"
 	"testing"
 
-	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/aetest"
 )
 
 func TestCheckVAT(t *testing.T) {
-	// TODO: Add passing case
+	// aetest.NewContext doesn't seem to satisfy urlfetch
+	inst, _ := aetest.NewInstance(nil)
+	defer inst.Close()
+	req, _ := inst.NewRequest(http.MethodGet, "/", nil)
+	ctx := appengine.NewContext(req)
+
 	cases := []struct {
 		VATID string
 		Valid bool
@@ -16,10 +23,11 @@ func TestCheckVAT(t *testing.T) {
 		{"RO    ", false},
 		{"AT0000", false},
 		{"RO1234", false},
+		{"DE146269081", true},
 	}
 
 	for _, test := range cases {
-		r := CheckVAT(context.Background(), test.VATID)
+		r := CheckVAT(ctx, test.VATID)
 		if r != test.Valid {
 			t.Fatalf("%v is %v, expected %v", test.VATID, r, test.Valid)
 		}
